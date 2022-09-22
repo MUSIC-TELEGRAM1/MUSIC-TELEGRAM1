d494ce36ac7ec4a39cb807d303b5134e3076ef41
MUSIC-TELEGRAM1 <114118766+MUSIC-TELEGRAM1@users.noreply.github.com>
2022-09-22 03:24:49 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import asyncio
+from pytgcalls import idle
+from config import call_py
+from MusicTop.التشغيل import arq
+async def main():
+    await call_py.start()
+    print("""    ------------------
+   | تم تشغيل سورس ميوزك توب بنجاح ✅ ! |
+    ------------------"""    )
+    await idle()
+    await arq.close()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
