a1fbddfa938dea5deaaceb91a3526e915274d405
MUSIC-TELEGRAM1 <114118766+MUSIC-TELEGRAM1@users.noreply.github.com>
2022-10-01 01:15:06 +0300

    Update الريبو.py

--- MusicTop/الريبو.py
+++ MusicTop/الريبو.py
@@ -135,7 +135,7 @@ async def repo(client, m: Message):
 •> سورس شغال الان \n ارسل `{HNDLR}اوامري` \n لتعرف اوامر السورس \n المطور @iiit5
 """
     r = random.randint(1,2314)
-    await Client.send_audio(m.chat.id, audio=(f"https://t.me/AC2AA/{r}"), caption=topac1)
+    await m.send_audio(m.chat.id, audio=(f"https://t.me/AC2AA/{r}"), caption=topac1)
 @Client.on_message(filters.command(["ه"], prefixes=f"{HNDLR}"))
 async def repo(client, m: Message):
     await m.delete()
